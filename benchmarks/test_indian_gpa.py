2bc647898d46e713836311e80ea4ba412467e0b9
Katherine Wu <katherinewu312@yahoo.com>
2025-04-24 03:04:51 -0400

    Add an example to benchmark (but still need to get the cdice version working)

---
+++ benchmarks/test_indian_gpa.py
@@ -0,0 +1,234 @@
+# Copyright 2020 MIT Probabilistic Computing Project.
+# See LICENSE.txt
+
+'''
+Indian GPA example from:
+
+Discrete-Continuous Mixtures in Probabilistic Programming: Generalized
+Semantics and Inference Algorithms, Wu et. al., ICML 2018.
+https://arxiv.org/pdf/1806.02027.pdf
+'''
+
+from sppl.compilers.ast_to_spe import Id
+from sppl.compilers.ast_to_spe import IfElse
+from sppl.compilers.ast_to_spe import Sample
+from sppl.compilers.ast_to_spe import Sequence
+from sppl.compilers.sppl_to_python import SPPL_Compiler
+from sppl.distributions import atomic
+from sppl.distributions import choice
+from sppl.distributions import uniform
+from sppl.math_util import allclose
+from sppl.sets import Interval
+from sppl.spe import ExposedSumSPE
+
+Nationality = Id('Nationality')
+Perfect     = Id('Perfect')
+GPA         = Id('GPA')
+
+def model_perfect_nested_compiled():
+    compiler = SPPL_Compiler('''
+Nationality   ~= choice({'India': 0.5, 'USA': 0.5})
+if (Nationality == 'India'):
+    Perfect       ~= choice({'True': 0.01, 'False': 0.99})
+    if (Perfect == 'False'):
+        GPA ~= uniform(loc=0, scale=10)
+    else:
+        GPA ~= atomic(loc=10)
+elif (Nationality == 'USA'):
+    Perfect       ~= choice({'True': 0.01, 'False': 0.99})
+    if (Perfect == 'False'):
+        GPA ~= uniform(loc=0, scale=4)
+    else:
+        GPA ~= atomic(loc=4)
+    ''')
+    namespace = compiler.execute_module()
+    return namespace.model
+
+
+
+
+
+
+def model_no_latents():
+    return \
+        0.5 * ( # American student
+            0.99 * (GPA >> uniform(loc=0, scale=4)) | \
+            0.01 * (GPA >> atomic(loc=4))) | \
+        0.5 * ( # Indian student
+            0.99 * (GPA >> uniform(loc=0, scale=10)) | \
+            0.01 * (GPA >> atomic(loc=10)))
+
+def model_exposed():
+    return ExposedSumSPE(
+        spe_weights=(Nationality >> choice({'India': 0.5, 'USA': 0.5})),
+        children={
+            # American student.
+            'USA': ExposedSumSPE(
+                spe_weights=(Perfect >> choice({'True': 0.01, 'False': 0.99})),
+                children={
+                    'False'   : GPA >> uniform(loc=0, scale=4),
+                    'True'    : GPA >> atomic(loc=4),
+                }),
+            # Indian student.
+            'India': ExposedSumSPE(
+                spe_weights=(Perfect >> choice({'True': 0.01, 'False': 0.99})),
+                children={
+                    'False'   : GPA >> uniform(loc=0, scale=10),
+                    'True'    : GPA >> atomic(loc=10),
+                })},
+        )
+
+def model_ifelse_exhuastive():
+    command = Sequence(
+        Sample(Nationality, choice({'India': 0.5, 'USA': 0.5})),
+        Sample(Perfect,     choice({'True': 0.01, 'False': 0.99})),
+        IfElse(
+            (Nationality << {'India'}) & (Perfect << {'False'}),
+                Sample(GPA, uniform(loc=0, scale=10))
+            ,
+            (Nationality << {'India'}) & (Perfect << {'True'}),
+                Sample(GPA, atomic(loc=10))
+            ,
+            (Nationality << {'USA'}) & (Perfect << {'False'}),
+                Sample(GPA, uniform(loc=0, scale=4))
+            ,
+            (Nationality << {'USA'}) & (Perfect << {'True'}),
+                Sample(GPA, atomic(loc=4))))
+    return command.interpret()
+
+def model_ifelse_non_exhuastive():
+    Nationality = Id('Nationality')
+    Perfect     = Id('Perfect')
+    GPA         = Id('GPA')
+    command = Sequence(
+        Sample(Nationality, choice({'India': 0.5, 'USA': 0.5})),
+        Sample(Perfect,     choice({'True': 0.01, 'False': 0.99})),
+        IfElse(
+            (Nationality << {'India'}) & (Perfect << {'False'}),
+                Sample(GPA, uniform(loc=0, scale=10))
+            ,
+            (Nationality << {'India'}) & (Perfect << {'True'}),
+                Sample(GPA, atomic(loc=10))
+            ,
+            (Nationality << {'USA'}) & (Perfect << {'False'}),
+                Sample(GPA, uniform(loc=0, scale=4))
+            ,
+            True,
+                Sample(GPA, atomic(loc=4))))
+    return command.interpret()
+
+def model_ifelse_nested():
+    Nationality = Id('Nationality')
+    Perfect     = Id('Perfect')
+    GPA         = Id('GPA')
+    command = Sequence(
+        Sample(Nationality, choice({'India': 0.5, 'USA': 0.5})),
+        Sample(Perfect,     choice({'True': 0.01, 'False': 0.99})),
+        IfElse(
+            Nationality << {'India'},
+                IfElse(
+                    Perfect << {'True'},    Sample(GPA, atomic(loc=10)),
+                    Perfect << {'False'},   Sample(GPA, uniform(scale=10)),
+                ),
+            Nationality << {'USA'},
+                IfElse(
+                    Perfect << {'True'},    Sample(GPA, atomic(loc=4)),
+                    Perfect << {'False'},   Sample(GPA, uniform(scale=4)),
+                )))
+    return command.interpret()
+
+def model_perfect_nested():
+    Nationality = Id('Nationality')
+    Perfect     = Id('Perfect')
+    GPA         = Id('GPA')
+    command = Sequence(
+        Sample(Nationality, choice({'India': 0.5, 'USA': 0.5})),
+        IfElse(
+            Nationality << {'India'}, Sequence(
+                    Sample(Perfect, choice({'True': 0.01, 'False': 0.99})),
+                    IfElse(
+                        Perfect << {'True'},    Sample(GPA, atomic(loc=10)),
+                        True,                   Sample(GPA, uniform(scale=10))
+                    )),
+            Nationality << {'USA'}, Sequence(
+                Sample(Perfect, choice({'True': 0.01, 'False': 0.99})),
+                IfElse(
+                    Perfect << {'True'},    Sample(GPA, atomic(loc=4)),
+                    True,                   Sample(GPA, uniform(scale=4)),
+                ))))
+    return command.interpret()
+
+def model_ifelse_exhuastive_compiled():
+    compiler = SPPL_Compiler('''
+Nationality   ~= choice({'India': 0.5, 'USA': 0.5})
+Perfect       ~= choice({'True': 0.01, 'False': 0.99})
+if (Nationality == 'India') & (Perfect == 'False'):
+    GPA ~= uniform(loc=0, scale=10)
+elif (Nationality == 'India') & (Perfect == 'True'):
+    GPA ~= atomic(loc=10)
+elif (Nationality == 'USA') & (Perfect == 'False'):
+    GPA ~= uniform(loc=0, scale=4)
+elif (Nationality == 'USA') & (Perfect == 'True'):
+    GPA ~= atomic(loc=4)
+    ''')
+    namespace = compiler.execute_module()
+    return namespace.model
+
+def model_ifelse_non_exhuastive_compiled():
+    compiler = SPPL_Compiler('''
+Nationality   ~= choice({'India': 0.5, 'USA': 0.5})
+Perfect       ~= choice({'True': 0.01, 'False': 0.99})
+if (Nationality == 'India') & (Perfect == 'False'):
+    GPA ~= uniform(loc=0, scale=10)
+elif (Nationality == 'India') & (Perfect == 'True'):
+    GPA ~= atomic(loc=10)
+elif (Nationality == 'USA') & (Perfect == 'False'):
+    GPA ~= uniform(loc=0, scale=4)
+else:
+    GPA ~= atomic(loc=4)
+    ''')
+    namespace = compiler.execute_module()
+    return namespace.model
+
+def model_ifelse_nested_compiled():
+    compiler = SPPL_Compiler('''
+Nationality   ~= choice({'India': 0.5, 'USA': 0.5})
+Perfect       ~= choice({'True': 0.01, 'False': 0.99})
+if (Nationality == 'India'):
+    if (Perfect == 'False'):
+        GPA ~= uniform(loc=0, scale=10)
+    else:
+        GPA ~= atomic(loc=10)
+elif (Nationality == 'USA'):
+    if (Perfect == 'False'):
+        GPA ~= uniform(loc=0, scale=4)
+    elif (Perfect == 'True'):
+        GPA ~= atomic(loc=4)
+    ''')
+    namespace = compiler.execute_module()
+    return namespace.model
+
+def model_perfect_nested_compiled():
+    compiler = SPPL_Compiler('''
+Nationality   ~= choice({'India': 0.5, 'USA': 0.5})
+if (Nationality == 'India'):
+    Perfect       ~= choice({'True': 0.01, 'False': 0.99})
+    if (Perfect == 'False'):
+        GPA ~= uniform(loc=0, scale=10)
+    else:
+        GPA ~= atomic(loc=10)
+elif (Nationality == 'USA'):
+    Perfect       ~= choice({'True': 0.01, 'False': 0.99})
+    if (Perfect == 'False'):
+        GPA ~= uniform(loc=0, scale=4)
+    else:
+        GPA ~= atomic(loc=4)
+    ''')
+    namespace = compiler.execute_module()
+    return namespace.model
+
+model = model_perfect_nested_compiled()
+GPA = Id('GPA')
+model_condition = model.condition(GPA >= 0)
+result = model.prob(GPA << {4})
+print(result)
